5ca77c0ae2ab1d0aa5315fa04e089068a563650b
Ben Briggs <bbriggs@hubspot.com>
2016-07-18 13:02:20 -0400

    add licensing to convertFromHTML

--- src/convertFromHTML.js
+++ src/convertFromHTML.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the /src directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 import {List, OrderedSet} from 'immutable';
 import invariant from 'invariant';
 import {ContentState, CharacterMetadata, ContentBlock, Entity, genKey} from 'draft-js';
