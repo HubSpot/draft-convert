38fc9c7dc47d07c82180ab440f8cbca1d97faad1
Ben Briggs <bbriggs@hubspot.com>
2016-07-19 15:47:24 -0400

    redo build process to not have a bin directory and to output both UMD and commonJS files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import convertToHTML from './convertToHTML';
+import convertFromHTML from './convertFromHTML';
+import parseHTML from './util/parseHTML';
+
+export {convertToHTML, convertFromHTML, parseHTML};
