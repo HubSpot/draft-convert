5e0087915be23cc963dc5407746778849ec62148
Ben Briggs <bbriggs@hubspot.com>
2017-04-20 11:28:48 -0400

    use json-loader for webpack build

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,6 +6,11 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: /node_modules/,
+    },
+    {
+      test: /\.json$/,
+      loader: 'json-loader',
+      exclude: /node_modules/,
     }]
   },
   output: {
