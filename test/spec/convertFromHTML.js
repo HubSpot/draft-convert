741adc654c6d9d4f3eb1233807a6fa08b9525965
Ben Briggs <bbriggs@hubspot.com>
2017-07-13 18:13:06 -0400

    remove unused variable

--- test/spec/convertFromHTML.js
+++ test/spec/convertFromHTML.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Entity, convertToRaw } from 'draft-js';
+import { convertToRaw } from 'draft-js';
 import convertFromHTML from '../../src/convertFromHTML';
 import convertToHTML from '../../src/convertToHTML';
 
