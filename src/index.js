70568b5534328a55072191c26533c3fe81a0c66b
Ben Briggs <bbriggs@hubspot.com>
2017-09-12 15:20:43 -0400

    remove package version export and json-loader dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import convertToHTML from './convertToHTML';
 import convertFromHTML from './convertFromHTML';
 import parseHTML from './util/parseHTML';
-import { version as __version } from '../package.json';
 
-export { convertToHTML, convertFromHTML, parseHTML, __version };
+export { convertToHTML, convertFromHTML, parseHTML };
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,11 +6,6 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: /node_modules/,
-    },
-    {
-      test: /\.json$/,
-      loader: 'json-loader',
-      exclude: /node_modules/,
     }]
   },
   output: {
